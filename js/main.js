b928b170c2d94fb06d43e01ee98f659c44d3af07
davemarkov <dmarkov91@gmail.com>
2018-07-26 17:12:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function changeBackground(color) {
     body.style.backgroundColor = color.value;
 }
 
-var searchBar = document.getElementById('search-form');
+//var searchBar = document.getElementById('search-form');
 
 //Add event listener
 searchBar.addEventListener('input', searchSite);
@@ -12,36 +12,6 @@ var sth = document.querySelector('.nav-and-title');
 var newElem = document.createElement('div');
 //console.log(newElem);
 //console.log(sth.parentNode);
-
-
-function searchSite() {
-
-    var searchValue = searchBar.value.toUpperCase();
-    //  console.log(searchValue);
-
-    //get all text-content
-    var contacts = document.querySelectorAll('div.text-content');
-    //console.log(contacts);
-    var searchBox = document.getElementById('display-search').style;
-    searchBox.display = 'block';
-    //loop through contacts
-    if (searchValue == '') {
-        document.getElementById('display-search').innerHTML = '';
-        document.getElementById('nav-and-title').style.position = 'fixed';
-        return;
-    }
-    document.getElementById('nav-and-title').style.position = 'relative';
-    for (var i = 0; i < contacts.length; i++) {
-        var text = contacts[i].getElementsByTagName('h1')[0];
-        //  console.log(contacts[i]);
-        if (text.innerHTML.toLocaleUpperCase().localeCompare(searchValue) > -1) {
-            //contacts[i].style.display = 'none';
-            document.getElementById('display-search').innerHTML = contacts[i];
-        } else {
-            document.getElementById('display-search').innerHTML = '';
-        }
-    }
-}
 
 function openHiddenContent(n,id) {
     var contentid = "hidden-content" + n;
