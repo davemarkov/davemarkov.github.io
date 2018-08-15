ba712e0b5e63a6bce2d3854100c881280cf7efa9
davemarkov <dmarkov91@gmail.com>
2018-08-15 13:33:40 +0300

    Fixed menu bar

--- js/main.js
+++ js/main.js
@@ -13,6 +13,36 @@ var newElem = document.createElement('div');
 //console.log(newElem);
 //console.log(sth.parentNode);
 
+
+//function searchSite() {
+//
+//    var searchValue = searchBar.value.toUpperCase();
+//    //  console.log(searchValue);
+//
+//    //get all text-content
+//    var contacts = document.querySelectorAll('div.text-content');
+//    //console.log(contacts);
+//    var searchBox = document.getElementById('display-search').style;
+//    searchBox.display = 'block';
+//    //loop through contacts
+//    if (searchValue == '') {
+//        document.getElementById('display-search').innerHTML = '';
+//        document.getElementById('nav-and-title').style.position = 'fixed';
+//        return;
+//    }
+//    document.getElementById('nav-and-title').style.position = 'relative';
+//    for (var i = 0; i < contacts.length; i++) {
+//        var text = contacts[i].getElementsByTagName('h1')[0];
+//        //  console.log(contacts[i]);
+//        if (text.innerHTML.toLocaleUpperCase().localeCompare(searchValue) > -1) {
+//            //contacts[i].style.display = 'none';
+//            document.getElementById('display-search').innerHTML = contacts[i];
+//        } else {
+//            document.getElementById('display-search').innerHTML = '';
+//        }
+//    }
+//}
+
 function openHiddenContent(n,id) {
     var contentid = "hidden-content" + n;
     id.setAttribute('onclick','closeHiddenContent(' + n + ',this)');
@@ -45,3 +75,12 @@ function closePopup(n) {
     var content = document.getElementById(contentid);
     content.style.display = 'none';
 }
+
+////////////////////////////////////////////////////////////////////
+
+
+const arr = [12,,34,62,45,23,56,23,32,56,23,54,3,6,21,5];
+
+
+var newArr = arr.filter(n => n > 30);
+console.log(newArr);
